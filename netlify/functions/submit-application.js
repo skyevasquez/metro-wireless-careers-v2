55e03a1df60bb4b7bd941269a156441d393bfaee
skyevasquez <skyevasquez@gmail.com>
2025-07-22 17:39:52 -0400

    Fix critical nodemailer method name error

--- netlify/functions/submit-application.js
+++ netlify/functions/submit-application.js
@@ -3,7 +3,7 @@ const multipart = require('lambda-multipart-parser');
 
 // Email configuration
 const createTransporter = () => {
-    return nodemailer.createTransporter({
+    return nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
--- netlify/functions/test-email.js
+++ netlify/functions/test-email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // Email configuration
 const createTransporter = () => {
-    return nodemailer.createTransporter({
+    return nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
